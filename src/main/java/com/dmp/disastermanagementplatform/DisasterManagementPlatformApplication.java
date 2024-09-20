6adefaa666f029c35c355c3dda9e5ab72c00c5f2
哈瑞斯联邦 <1070236799@qq.com>
2024-09-20 11:09:13 +0800

    first commit

---
+++ src/main/java/com/dmp/disastermanagementplatform/DisasterManagementPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.dmp.disastermanagementplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisasterManagementPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DisasterManagementPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dmp/disastermanagementplatform/DisasterManagementPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmp.disastermanagementplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisasterManagementPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
